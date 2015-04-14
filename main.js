297e712f5e34f04321dad5f1bb654d6628b073e9
Robin Schmid <robin@seerow.ch>
2015-04-14 10:36:21 +0200

    added fades animation

--- main.js
+++ main.js
@@ -5,7 +5,17 @@ $(document).ready(function()
 {
     $("#image-1").hover(
         function () {
-            $("#image-description").text("Dies ist Bild 1.");
+            $("#image-description").fadeOut("slow", function () {
+                $("#image-description").html("<p>Dies ist Bild 1.<br>Dies ist die 2. Zeile</p>").fadeIn("slow")
+            });
+        }
+    );
+
+    $("#image-2").hover(
+        function () {
+            $("#image-description").fadeOut("slow", function () {
+                $("#image-description").html("<p>Dies ist Bild 2.<br>Dies ist die 2. Zeile</p>").fadeIn("slow")
+            });
         }
     );
 });
