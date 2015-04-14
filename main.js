4467ee54f9ef574a4c37dc6331b58936a9863cfb
Jona <jonaklar98@gmail.com>
2015-04-14 11:43:32 +0200

    added stuff

--- main.js
+++ main.js
@@ -3,19 +3,76 @@
  */
 $(document).ready(function()
 {
-    $("#image-1").hover(
+    $("#image-1").mouseenter(
         function () {
-            $("#image-description").fadeOut("slow", function () {
-                $("#image-description").html("<p>Dies ist Bild 1.<br>Dies ist die 2. Zeile</p>").fadeIn("slow")
+            $("#image-description").fadeOut(400, function () {
+                $("#image-description").html("<p>Schwarzes Designerkleid, kurzärmlig und knielang.<br>Grössen: S/M/L<br>80.- Fr.</p>").fadeIn(400)
             });
         }
     );
 
-    $("#image-2").hover(
+    $("#image-2").mouseenter(
         function () {
-            $("#image-description").fadeOut("slow", function () {
-                $("#image-description").html("<p>Dies ist Bild 2.<br>Dies ist die 2. Zeile</p>").fadeIn("slow")
+            $("#image-description").fadeOut(400, function () {
+                $("#image-description").html("<p>Schwarzes Suitset mit weissem Hemd und tiefgeschnittenem Blazer.<br>Grössen: XS/S/M/L/XL<br>260.- Fr.</p>").fadeIn(400)
             });
         }
     );
+    
+     $("#image-3").mouseenter(
+        function () {
+            $("#image-description").fadeOut(400, function () {
+                $("#image-description").html("<p>Weisse Echtlederjacke und Grau melierte Anzugshose.<br>Grössen: M/L/XL<br>375.- Fr.</p>").fadeIn(400)
+            });
+        }
+    );
+    
+     $("#image-4").mouseenter(
+        function () {
+            $("#image-description").fadeOut(400, function () {
+                $("#image-description").html("<p>Schwarze Echtlederjacke mit skinny Hose, silber, Polyester.<br>Grössen: XS/S/L/XL<br>180.- Fr.</p>").fadeIn(400)
+            });
+        }
+    );
+    
+     $("#image-5").mouseenter(
+        function () {
+            $("#image-description").fadeOut(400, function () {
+                $("#image-description").html("<p>Weiss-Schwarz melierter Pullover mit Schwarzem Fellflies, kurzärmlig und Leggins, weinrot.<br>Grössen: S/M/L<br>230.- Fr.</p>").fadeIn(400)
+            });
+        }
+    );
+    
+     $("#image-6").mouseenter(
+        function () {
+            $("#image-description").fadeOut(400, function () {
+                $("#image-description").html("<p>T-Shirt rot gefleckt mit schwarzen Lederhosen, skinny.<br>XS/S/M<br>90.- Fr.</p>").fadeIn(400)
+            });
+        }
+    );
+    
+     $("#image-7").mouseenter(
+        function () {
+            $("#image-description").fadeOut(400, function () {
+                $("#image-description").html("<p>Trägerkleid, silber, lang.<br>Grössen: M/L<br>135.- Fr.</p>").fadeIn(400)
+            });
+        }
+    );
+    
+     $("#image-8").mouseenter(
+        function () {
+            $("#image-description").fadeOut(400, function () {
+                $("#image-description").html("<p>kurzärmliges, orangenes Kleid, kurz, V-Ausschnitt, Baumwolle.<br>Grössen: M/L/XL<br>75.- Fr.</p>").fadeIn(400)
+            });
+        }
+    );
+    
+     $("#image-9").mouseenter(
+        function () {
+            $("#image-description").fadeOut(400, function () {
+                $("#image-description").html("<p>schwarzer Pelzpullover, lang.<br>Grössen: S/M/L<br>110.- Fr.</p>").fadeIn(400)
+            });
+        }
+    );
+    
 });
